860cc745561746c5581f870f4e5686b5922ae0b6
Bhanu Katoch <bhanukatoch445@gmail.com>
2026-04-09 22:55:42 +0530

    Add: maximum-frequency-stack - submission-1

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-1.java
@@ -0,0 +1,32 @@
+class FreqStack {
+    Map<Integer, Integer> freqMap = new HashMap<>(); // Stores: Value -> Current Frequency
+    Map<Integer, Deque<Integer>> groupMap = new HashMap<>(); // Stores: Frequency -> Stack of elements
+    int maxFreq = 0;
+
+    public void push(int val) {
+        // 1. Update frequency for this value
+        int f = freqMap.getOrDefault(val, 0) + 1;
+        freqMap.put(val, f);
+
+        // 2. Keep track of the highest frequency globally
+        if (f > maxFreq) maxFreq = f;
+
+        // 3. Add the value to the stack corresponding to its frequency
+        groupMap.computeIfAbsent(f, k -> new ArrayDeque<>()).push(val);
+    }
+
+    public int pop() {
+        // 1. Get the most recent element from the highest frequency group
+        int val = groupMap.get(maxFreq).pop();
+
+        // 2. Decrement the frequency in our map
+        freqMap.put(val, maxFreq - 1);
+
+        // 3. If that frequency group is now empty, move down to the next frequency
+        if (groupMap.get(maxFreq).isEmpty()) {
+            maxFreq--;
+        }
+
+        return val;
+    }
+}
