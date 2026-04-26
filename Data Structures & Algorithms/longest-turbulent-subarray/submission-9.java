2cc23bc76dc32b309a35dba22388749928c0446b
Bhanu Katoch <bhanukatoch445@gmail.com>
2026-04-26 17:14:17 +0530

    Add: longest-turbulent-subarray - submission-9

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-9.java
@@ -0,0 +1,71 @@
+class Solution {
+    int arr[];
+    public int maxTurbulenceSize(int[] arr) {
+        this.arr = arr;
+        int n = arr.length;
+        int max = 1;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(j>i && isTurbulent(i,j))
+                    max = Math.max(max,j-i+1);
+            }
+        }
+        return max;
+    }
+    private boolean isTurbulent(int start, int end){
+        boolean flag = true;
+        for(int i=start;i<=end;i++){
+            if((i-start)%2==0){
+                if(i<end){
+                    if(arr[i+1]<arr[i])
+                        continue;
+                    else{
+                        flag = false;
+                        break;
+                    }
+                } 
+                else continue;
+                
+            }
+            else{
+                if(i<end){
+                    if(arr[i+1]>arr[i])
+                        continue;
+                    else{
+                        flag = false;
+                        break;
+                    }
+                }
+                else continue;
+            }
+        }
+        if(flag==true) return true;
+        flag = true;
+        for(int i=start;i<=end;i++){
+            if((i-start)%2==0){
+                if(i<end){
+                    if(arr[i+1]>arr[i])
+                        continue;
+                    else{
+                        flag = false;
+                        break;
+                    }
+                }     
+                else continue;
+            }
+                
+            else{
+                if(i<end){
+                     if(arr[i+1]<arr[i])
+                        continue;
+                    else{
+                        flag = false;
+                        break;
+                    }
+                }
+                else continue;
+            }
+        }
+        return flag;
+    }
+}
